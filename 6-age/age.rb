170197aafa1c6fe39962d5d07b50d3a2df550815
Gabriel Pereira <gabrielpereira@MacBook-Air-de-Gabriel.local>
2024-01-14 11:19:54 -0300

    app to find out age

---
+++ 6-age/age.rb
@@ -0,0 +1,30 @@
+require 'date'
+result = ''
+
+loop do 
+    puts result
+    puts 'Selecione uma das seguintes opções'
+    puts '1- Descobrir a idade de uma pessoa'
+    puts '0- Sair'
+    puts 'Opção: '
+
+    option = gets.chomp.to_i
+
+    if option == 1
+        print 'Digite o ano de nascimento: '
+        year_of_birth = gets.chomp.to_i
+
+        current_year = Date.today.year
+
+        age = current_year - year_of_birth
+
+        result = "Quem nasceu no ano de #{year_of_birth} tem ou vai ter #{age} anos no ano de #{current_year}"
+    elsif option == 0
+        break
+    else
+        result = 'Opção inválida'
+    end
+    # comando que limpa o console
+    system 'clear'
+
+end
