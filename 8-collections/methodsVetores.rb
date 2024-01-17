d5bec7c26491bd470cabb427a4f09e4fa9d346c6
gabriel <gabriel.souza@interativachat.com.br>
2024-01-16 22:05:01 -0300

    methods of vetores

--- 8-collections/methodsVetores.rb
+++ 8-collections/methodsVetores.rb
@@ -1,15 +1,21 @@
 states = ['São Paulo', 'Rio de Janeiro', 'Bahia']
 print states
 
+
+# add
+
 states.push('Minas Gerais')
 # .push adiciona itens no array
-print states
 
+# consulta
 states.first
 # .first retorna o primeiro elemento do vetor (0)
 
 states.last
 # .last retorna o ultimo elemento do vetor (-1)
+
+states.size
+# .size retorna a quantidade de elementos em um vetor
 
 states.empty?
 # .empty? verifica se o vetor está vazio e retorna true
@@ -17,3 +23,13 @@ states.empty?
 states.include?('Bahia')
 # .include? busca por um item dentro do vetor e retorna true ou false
 
+
+#remove
+states.delete_at(1)
+# .delete_at remove o elemento na posição indicada
+
+states.pop
+# .pop remove o último elemento do vetor
+
+states.shift
+# .shift remove o primeiro elemento do vetor
