85ccfae675be355862e1d9543024ca50c8219770
Gabriel Pereira <gabrielpereira@MacBook-Air-de-Gabriel.local>
2024-01-19 08:21:58 -0300

    methods

---
+++ 9-methods/methods.rb
@@ -0,0 +1,13 @@
+def talk name, my_name, age, city
+    puts "Iae #{name} tudo certo? meu nome é #{my_name} tenho #{age}, e moro em #{city}."
+end
+
+talk "Luiz", "Gabriel", 24, "BH"
+# a criação e execução do metodo pode ser sem ()
+
+
+def talk(name, my_name, age, city)
+    puts "Iae #{name} tudo certo? meu nome é #{my_name} tenho #{age}, e moro em #{city}."
+end
+
+talk("João", "Souzinha", 24, "Betim")
