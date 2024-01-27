d1e1324bb54f3b494344f73a9cab07e257050103
Gabriel Pereira <gabrielpereira@MacBook-Air-de-Gabriel.local>
2024-01-27 09:06:26 -0300

    brock

--- 13-block/block.rb
+++ 13-block/block.rb
@@ -5,7 +5,7 @@ sum = 0
 num = numbers = [5, 10, 7]
 numbers.each {|number| sum += number}
 puts sum
-puts '-----'
+puts '======='
 # -----
 
 foo = { 'nome' => 3, 'age' => 5}
@@ -24,3 +24,37 @@ def foo2
 end
 
 foo2 { puts "Exec the block 2"}
+
+puts '======'
+
+def foo3 
+    if block_given?
+
+        yield
+    else
+        puts 'sem parâmetro'
+    end
+end
+
+
+foo3
+foo3 { puts "Exec o bloco com parâmetro" }
+
+puts '====='
+
+def foo4(numbers, &block)
+    if block_given?
+        numbers.each do |key,value|
+            block.call(key,value)
+        end
+    end
+end
+
+
+numbers = { 2 => 2, 3 => 3, 4 => 4 }
+
+foo4(numbers) do |key, value|
+    puts "#{key} * #{value} = #{key * value}"
+    puts "#{key} + #{value} = #{key + value}"
+    puts '---'
+end
