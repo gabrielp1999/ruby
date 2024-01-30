0887cb8fa45b7ad12a9366d808ac9cc5a47a19e7
Gabriel Pereira <gabrielpereira@MacBook-Air-de-Gabriel.local>
2024-01-29 23:13:25 -0300

    self

---
+++ 18-self/self.rb
@@ -0,0 +1,21 @@
+class Foo
+    def bar 
+        puts self
+    end
+end
+
+foo = Foo.new
+puts foo
+foo.bar
+
+class Pen
+    attr_accessor :color
+    def pen_color
+        puts  "The color of the pen is #{self.color}"
+        # tudo que a instancia tem acesso o self também tem
+    end
+end
+
+pen = Pen.new
+pen.color = 'blue'
+pen.pen_color
