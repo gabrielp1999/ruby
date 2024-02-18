96b5ffecdb1bc4991f5a766eea8657fa1e2498b6
Gabriel Pereira <gabrielpereira@MacBook-Air-de-Gabriel.local>
2024-02-18 13:14:43 -0300

    web scraping posts

---
+++ 23-web-scrap/web_scrap_post.rb
@@ -0,0 +1,17 @@
+require 'nokogiri'
+require 'net/http'
+
+https = Net::HTTP.new('www.onebitcode.com', 443)
+
+https.use_ssl = true
+response = https.get('/')
+puts response.header['location']
+
+doc = Nokogiri::HTML(response.body)
+
+doc.search('a').each do |a|
+    puts a.content
+    puts a['href']
+    puts '===='
+end
+
